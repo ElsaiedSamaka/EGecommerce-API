9fa49045ca43fc8ddebc1f0857b12fab6e1d52ea
Elsaied samaka <elsaied.samaka1997@gmail.com>
2022-11-19 08:55:37 +0200

    refactor product model

--- models/Product.model.js
+++ models/Product.model.js
@@ -2,6 +2,10 @@ const Product = require("../models/Product.model");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// When you create a Electronic Product, it will set the type to Eletronic. 
+// When you create a Clothes Product, it will set the type to Clothes. 
+let options = { discriminatorKey: 'type' };
+// parent Product schema.
 const productSchema = Schema({
   title: {
     type: String,
@@ -19,13 +23,8 @@ const productSchema = Schema({
     type: Number,
     required: true,
   },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
-  },
-  manufacturer: {
-    type: String,
-  },
+  
+ 
   available: {
     type: Boolean,
     required: true,
@@ -45,6 +44,20 @@ const productSchema = Schema({
     type:Number,
     default: 0
   }
-});
+}, options);
 
-module.exports = mongoose.model("Product", productSchema);
+// child Electronic schema.
+let ElectronicProduct = productSchema.discriminator('Electronic', new mongoose.Schema({ cpu: Number , manufacturer: {
+  type: String,
+}, }, options));
+
+// child Clothing schema. 
+let ClothingProduct = productSchema.discriminator('Clothing', new mongoose.Schema({ size: String  ,category: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: "Category",
+},}, options));
+
+module.exports = {
+  ElectronicProduct: mongoose.model("ElectronicProduct",ElectronicProduct),
+  ClothingProduct: mongoose.model("ClothingProduct",ClothingProduct)
+}
