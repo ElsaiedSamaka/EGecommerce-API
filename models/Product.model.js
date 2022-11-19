76bf0919b54742a87e104f55d34834f6ea3ed58e
Elsaied samaka <elsaied.samaka1997@gmail.com>
2022-11-19 11:26:04 +0200

    working version

--- models/Product.model.js
+++ models/Product.model.js
@@ -2,9 +2,7 @@ const Product = require("../models/Product.model");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// When you create a Electronic Product, it will set the type to Eletronic. 
-// When you create a Clothes Product, it will set the type to Clothes. 
-let options = { discriminatorKey: 'type' };
+
 // parent Product schema.
 const productSchema = Schema({
   title: {
@@ -24,7 +22,10 @@ const productSchema = Schema({
     required: true,
   },
   
- 
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
   available: {
     type: Boolean,
     required: true,
@@ -44,20 +45,7 @@ const productSchema = Schema({
     type:Number,
     default: 0
   }
-}, options);
+});
 
-// child Electronic schema.
-let ElectronicProduct = productSchema.discriminator('Electronic', new mongoose.Schema({ cpu: Number , manufacturer: {
-  type: String,
-}, }, options));
 
-// child Clothing schema. 
-let ClothingProduct = productSchema.discriminator('Clothing', new mongoose.Schema({ size: String  ,category: {
-  type: mongoose.Schema.Types.ObjectId,
-  ref: "Category",
-},}, options));
-
-module.exports = {
-  ElectronicProduct: mongoose.model("ElectronicProduct",ElectronicProduct),
-  ClothingProduct: mongoose.model("ClothingProduct",ClothingProduct)
-}
+module.exports = mongoose.model("Product", productSchema);
